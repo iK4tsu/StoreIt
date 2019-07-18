069e80bbcb7d82b301189203f2b0e48affbfd792
iK4tsu <joao.m.lourenco@hotmail.com>
2019-07-18 12:02:15 +0100

    Changed having multiple windows to only one

--- source/manageTitles.d
+++ source/manageTitles.d
@@ -11,6 +11,34 @@ import std.array : split;
 import source.settings;
 import source.functions;
 
+pragma(inline, true);
+void drawbox(WINDOW* manageScreen, int y, int x,
+				chtype left, chtype right, chtype top, chtype bottom,
+				chtype tlc, chtype blc, chtype trc, chtype brc, int vn, int hn)
+{
+	mvwhline(manageScreen, y, x, tlc, 1);							// top left corner
+	mvwhline(manageScreen, y + vn - 1, x, blc, 1);					// bottom left corner
+	mvwhline(manageScreen, y, x + hn - 1, trc, 1);					// top right corner
+	mvwhline(manageScreen, y + vn - 1, x + hn - 1, brc, 1);			// bottom right corner
+
+	mvwvline(manageScreen, y + 1, x, left, vn - 2);					// left line
+	mvwvline(manageScreen, y + 1, x + hn - 1, right, vn - 2);		// right line
+	mvwhline(manageScreen, y, x + 1, top, hn - 2);					// top line
+	mvwhline(manageScreen, y + vn - 1, x + 1, bottom, hn - 2);		// bottom line
+}
+
+pragma(inline, true);
+void drawlmanagescreen(WINDOW* manageScreen)
+{
+	for (int y = 1; y < 7; y++)
+	{
+		drawbox(manageScreen, y*3-1, 2, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_LLCORNER, ACS_TTEE, ACS_BTEE, 3, 20);
+		drawbox(manageScreen, y*3-1, 2 + 19, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_TTEE, ACS_BTEE, ACS_URCORNER, ACS_LRCORNER, 3, 80);
+	}
+
+	wrefresh(manageScreen);
+}
+
 bool manage(ref Mywindow manageScreen, string[] titles, ref Settings settings, int highlight, string option)
 {
 	auto path = settings.dir[highlight] ~ ".json";
@@ -18,15 +46,14 @@ bool manage(ref Mywindow manageScreen, string[] titles, ref Settings settings, i
 
 	JSONValue j;
 
+	Mywindow error = newWindow(6, 40, manageScreen.height / 2 - 6 / 2, manageScreen.width / 2 - 40 / 2, false);
+
 	//path = "not";
 
 	newBox(manageScreen.win, 0, 0);
 
 	if (!path.exists() && cmp(option, "Add") != 0)
 	{
-		Mywindow error = newWindow(6, 40,
-								manageScreen.height / 2 - 6 / 2,
-								manageScreen.width / 2 - 40 / 2, false);
 		newBox(error.win, 0, 0);
 		mvwprintcentery(error.win, error.width, error.height / 2 - 1, "File does not exist!");
 		mvwprintcentery(error.win, error.width, error.height / 2, "Add a title to be able to \"" ~ option ~ "\"");
@@ -43,7 +70,7 @@ bool manage(ref Mywindow manageScreen, string[] titles, ref Settings settings, i
 	}
 	else
 	{
-		string s = "{ \"AnimeDetails\":[{}] }";
+		string s = "{ \"AnimeDetails\":[] }";
 		j = parseJSON(s);
 	}
 
@@ -56,103 +83,68 @@ bool manage(ref Mywindow manageScreen, string[] titles, ref Settings settings, i
 
 		string single;
 
-		int stdscrY, stdscrX;
-		getmaxyx(stdscr, stdscrY, stdscrX);
-
-		//WINDOW* test = newwin(3, 80, 4, 4);
-		//box(test, 0, 0);
-		//refresh();
-		//wrefresh(test);
-		//mvwprintw(test, 1, 1, "Y: %d\tX: %d", stdscrY, stdscrX);
-		//wgetch(test);
-
-		Mywindow name = newWindow(3, 80, 4, 4, true);
-		Mywindow season = newWindow(3, 80, 7, 4, true);
-		Mywindow seasonName = newWindow(3, 80, 10, 4, true);
-		Mywindow episodes = newWindow(3, 80, 13, 4, true);
-		Mywindow status = newWindow(3, 80, 16, 4, true);
-		Mywindow curEp = newWindow(3, 80, 19, 4, true);
-
-		newBox(name.win, 0, 0);
-		newBox(season.win, 0, 0);
-		newBox(seasonName.win, 0, 0);
-		newBox(episodes.win, 0, 0);
-		newBox(status.win, 0, 0);
-		newBox(curEp.win, 0, 0);
-
-
 		string parse = "{ \"name\":\"\",
 							\"id_imdb\":0,
 							\"seasons\":[
 								{
-									\"season\":\"\",
+									\"manageScreen\":\"\",
 									\"season_name\":\"\",
 									\"episodes\":0,
 									\"official_status\":\"\",
 									\"year\":0,
 									\"season_year\":\"\",
 									\"status\":\"\",
-									\"episode\":0
+									\"cur_episode\":0
 								}],
-							\"genre\":[{}]}";
+							\"genre\":[]}";
 
-		j["AnimeDetails"].array ~= parseJSON(parse);
+		drawlmanagescreen(manageScreen.win);
+
+		mvwprintw(manageScreen.win, 3, 3, toStringz("Name: "));
+		mvwprintw(manageScreen.win, 6, 3, toStringz("Season: "));
+		mvwprintw(manageScreen.win, 9, 3, toStringz("SeasonName: "));
+		mvwprintw(manageScreen.win, 12, 3, toStringz("Episodes: "));
+		mvwprintw(manageScreen.win, 15, 3, toStringz("Status: "));
+		mvwprintw(manageScreen.win, 18, 3, toStringz("Current Episode: "));
 
 		foreach(string title; titles)
 		{
-			mvwprintw(name.win, 1, 1, toStringz(blank));
-			mvwprintw(season.win, 1, 1, toStringz(blank));
-			mvwprintw(seasonName.win, 1, 1, toStringz(blank));
-			mvwprintw(episodes.win, 1, 1, toStringz(blank));
-			mvwprintw(status.win, 1, 1, toStringz(blank));
-			mvwprintw(curEp.win, 1, 1, toStringz(blank));
+			j["AnimeDetails"].array ~= parseJSON(parse);
 
-			mvwprintw(name.win, 1, 1, toStringz("Name: "));
-			mvwprintw(season.win, 1, 1, toStringz("Season: "));
-			mvwprintw(seasonName.win, 1, 1, toStringz("SeasonName: "));
-			mvwprintw(episodes.win, 1, 1, toStringz("Episodes: "));
-			mvwprintw(status.win, 1, 1, toStringz("Status: "));
-			mvwprintw(curEp.win, 1, 1, toStringz("Current Episode: "));
+			mvwprintw(manageScreen.win, 3, 24, toStringz(blank));
+			mvwprintw(manageScreen.win, 6, 24, toStringz(blank));
+			mvwprintw(manageScreen.win, 9, 24, toStringz(blank));
+			mvwprintw(manageScreen.win, 12, 24, toStringz(blank));
+			mvwprintw(manageScreen.win, 15, 24, toStringz(blank));
+			mvwprintw(manageScreen.win, 18, 24, toStringz(blank));
 
-			mvwprintw(name.win, 1, 20, toStringz(title));
+			mvwprintw(manageScreen.win, 3, 24, toStringz(title));
+			wrefresh(manageScreen.win);
 
-			wrefresh(name.win);
-			wrefresh(season.win);
-			wrefresh(seasonName.win);
-			wrefresh(episodes.win);
-			wrefresh(status.win);
-			wrefresh(curEp.win);
+			wmove(manageScreen.win, 6, 24);
+			single = getstring(manageScreen.win);
+			j["AnimeDetails"].array[$ - 1]["seasons"].array[$ - 1]["manageScreen"].integer = to!(int)(single);
 
-			wmove(season.win, 1, 20);
-			input = "";
-			wgetstr(season.win, input.ptr);
-			single = to!(string)(input);
-			single = (single.chomp().split("\0"))[0];
+			wmove(manageScreen.win, 9, 24);
+			single = getstring(manageScreen.win);
+			j["AnimeDetails"].array[$ - 1]["seasons"].array[$ - 1]["season_name"].str = single;
 
-			wmove(seasonName.win, 1, 20);
-			input = "";
-			wgetstr(seasonName.win, input.ptr);
-			single = to!(string)(input);
-			single = (single.chomp().split("\0"))[0];
+			wmove(manageScreen.win, 12, 24);
+			single = getstring(manageScreen.win);
+			j["AnimeDetails"].array[$ - 1]["seasons"].array[$ - 1]["episodes"].integer = to!(int)(single);
 
-			wmove(episodes.win, 1, 20);
-			input = "";
-			wgetstr(episodes.win, input.ptr);
-			single = to!(string)(input);
-			single = (single.chomp().split("\0"))[0];
+			wmove(manageScreen.win, 15, 24);
+			single = getstring(manageScreen.win);
+			j["AnimeDetails"].array[$ - 1]["seasons"].array[$ - 1]["status"].str = single;
 
-			wmove(status.win, 1, 20);
-			input = "";
-			wgetstr(status.win, input.ptr);
-			single = to!(string)(input);
-			single = (single.chomp().split("\0"))[0];
+			wmove(manageScreen.win, 18, 24);
+			single = getstring(manageScreen.win);
+			j["AnimeDetails"].array[$ - 1]["seasons"].array[$ - 1]["cur_episode"].integer = to!(int)(single);
 
-			wmove(curEp.win, 1, 20);
-			input = "";
-			wgetstr(curEp.win, input.ptr);
-			single = to!(string)(input);
-			single = (single.chomp().split("\0"))[0];
+			j["AnimeDetails"].array[$ - 1]["name"].str = title;
 		}
+
+		write("source/" ~ path, j.toString);
 	}
 
 	//mvwprintw(manageScreen.win, 1, 1, toStringz(j["name"].str));
