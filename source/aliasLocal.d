c1801562c03bb7a2cd1fa95d60453cd3e756889e
iK4stu <joao.m.lourenco@hotmail.com>
2019-07-31 00:01:02 +0100

    Added color and color pairs

--- source/aliasLocal.d
+++ source/aliasLocal.d
@@ -2,6 +2,8 @@ module source.aliasLocal;
 
 import riverd.ncurses;
 
+import source.functions;
+
 /* special keys */
 struct KEYS
 {
@@ -24,6 +26,23 @@ struct COLORS
 	static immutable int silver = 16;
 	static immutable int grey = 17;
 	static immutable int lightgrey = 18;
+	static immutable int prussianblue = 19;
+	static immutable int capri = 20;
+	
+	/* comtemporary and bold */
+	static immutable int blackrussian = 21;
+	static immutable int claret = 22;
+	static immutable int tyrianpurple = 23;
+	static immutable int cardinal = 24;
+	static immutable int gunpowder = 25;
+
+	/* striking and simple */
+	static immutable int blackrussian2 = 26;
+	static immutable int bunker = 27;
+	static immutable int linkwater = 28;
+	static immutable int babyblue = 29;
+	static immutable int bluechill = 30;
+	static immutable int blackpearl = 31;
 }
 
 /* personalized color alias */
@@ -33,8 +52,19 @@ alias COLOR = COLORS;
 /* color pair id's */
 struct PAIRS
 {
-	static immutable int stdscr = 1;
-	static immutable int stdscrbtn = 2;
+	static immutable int stdscr = 1;			// standard screen
+	static immutable int stdscrbtn = 2;			// standard screen buttons
+	static immutable int stdscrbtn2 = 3;		// statdard screen button color 2
+
+	static immutable int barstdscr = 6;			// main bar
+	static immutable int barstdsmb = 7;			// bar diamond symbol
+	static immutable int barstdtxt = 8;			// bar text
+	static immutable int barstdslct = 9;		// bar selection
+	static immutable int barstdtxtslct = 10;	// bar text selection
+	
+	static immutable int mainmenu = 11;			// main menu window
+	static immutable int mainmenubtn = 12;		// main menu buttons
+	static immutable int mainmenubtnhlt = 13;	// main menu button highlight
 }
 
 /* color pair alias */
