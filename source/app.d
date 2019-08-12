9e5f3517e257f537b180d92d86da0952a3ab1839
iK4tsu <joao.m.lourenco@hotmail.com>
2019-08-12 18:22:54 +0100

    Added new windows

--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-module source.app;
+module app;
 
 import riverd.ncurses;
 import d2sqlite3;
@@ -8,13 +8,13 @@ import std.conv : to;
 import std.process : environment;
 import std.algorithm : cmp;
 
-import source.titlesMenu;
-import source.list;
-import source.functions;
-import source.settings;
-import source.manageTitles;
-import source.draw;
-import source.aliasLocal : COLOR_PAIR, PAIR;
+import titlesMenu;
+import list;
+import functions;
+import settings;
+import manageTitles;
+import draw;
+import aliasLocal : COLOR_PAIR, PAIR;
 import dbtable;
 
 void main()
@@ -29,13 +29,16 @@ void main()
 	WINDOW* mainmenu = newwin(to!(int)(2 + settings.categories.length), 20, getbegy(barstdscr) + 1, getbegx(barstdscr) + 3);
 	WINDOW* titleopmenu = newwin(5, 20, getbegy(mainmenu), getbegx(mainmenu));
 	WINDOW* fileopmenu = newwin(5, 20, getbegy(mainmenu), getbegx(barstdscr) + 37);
-
+	WINDOW* mngopmenu = newwin(to!(int)(getmaxy(stdscr) / 2), to!(int)(getmaxx(stdscr) / 2), getmaxy(stdscr) / 4, getmaxx(stdscr) / 4);
+	WINDOW* barmngmenu = newwin(1, to!(int)(getmaxx(mngopmenu) - 2), to!(int)(getbegy(mngopmenu) + 1), to!(int)(getbegx(mngopmenu) + 1));
 
 	/* init Mywindows */
 	Mywindow bar = properties(barstdscr, true);
 	Mywindow main = properties(mainmenu, true);
 	Mywindow tltop = properties(titleopmenu, true);
 	Mywindow fileop = properties(fileopmenu, true);
+	Mywindow mngop = properties(mngopmenu, true);
+	Mywindow barmng = properties(barmngmenu, true);
 
 
 	/* all windows */
@@ -67,7 +70,18 @@ void main()
 			continue;
 
 		/* titles to manage */
-		titles = tmenu(titleScreen, option);
+		// titles = tmenu(titleScreen, option);
+		switch (main.highlight)
+		{
+			case 0:
+				import dataManagement : animeData;
+				import draw : drawHomeScreen;
+				drawHomeScreen(barstdscr);
+				animeData(animesdb, option, mngop, barmng);
+				break;
+			
+			default:
+		}
 
 		/* user canceled the operation or didn't write anything */
 		if (titles.length == 0)
